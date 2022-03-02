8b8dcd5143685ebbf672bbb81f63f3d2d57b8794
Marco van Dijk <marco@stronk.rocks>
2022-03-02 17:27:53 +0100

    Precision of LPT and ETH v alues

--- src/eventButton.js
+++ src/eventButton.js
@@ -4,19 +4,19 @@ const EventButton = (obj) => {
   let eventSpecificInfo;
   if (obj.name == "EarningsClaimed") {
     eventSpecificInfo = <div className="row">
-      <p>(Round {obj.data.endRound}) Claim: {obj.data.delegator} earned {obj.data.rewards / 1000000000000000000} Eth @ Orchestrator {obj.data.delegate}</p>
+      <p>(Round {obj.data.endRound}) Claim: {obj.data.delegator} earned {(obj.data.rewards / 1000000000000000000).toFixed(4)} Eth @ Orchestrator {obj.data.delegate}</p>
     </div>
   } else if (obj.name == "Unbond") {
     eventSpecificInfo = <div className="row">
-      <p>(Round {obj.data.withdrawRound}) Unbond: {obj.data.delegator} unbonded {obj.data.amount / 1000000000000000000} Eth @ Orchestrator {obj.data.delegate}</p>
+      <p>(Round {obj.data.withdrawRound}) Unbond: {obj.data.delegator} unbonded {(obj.data.amount / 1000000000000000000).toFixed(4)} Eth @ Orchestrator {obj.data.delegate}</p>
     </div>
   } else if (obj.name == "TransferBond") {
     eventSpecificInfo = <div className="row">
-      <p>TransferBond: transfered bond worth {obj.data.amount / 1000000000000000000} Eth from {obj.data.oldDelegator} to {obj.data.newDelegator}</p>
+      <p>TransferBond: transfered bond worth {(obj.data.amount / 1000000000000000000).toFixed(4)} Eth from {obj.data.oldDelegator} to {obj.data.newDelegator}</p>
     </div>
   } else if (obj.name == "Bond") {
     eventSpecificInfo = <div className="row" style={{ backgroundColor: 'rgba(50,50,50,0.3)' }}>
-      <p>{obj.data.delegator} delegated {obj.data.bondedAmount / 1000000000000000000} LPT from {obj.data.oldDelegate} to {obj.data.newDelegate}</p>
+      <p>{obj.data.delegator} delegated {(obj.data.bondedAmount / 1000000000000000000).toFixed(4)} LPT from {obj.data.oldDelegate} to {obj.data.newDelegate}</p>
     </div>
   } else if (obj.name == "Rebond") {
     eventSpecificInfo = <div className="row">
@@ -28,14 +28,14 @@ const EventButton = (obj) => {
     </div>
   } else if (obj.name == "WithdrawStake") {
     eventSpecificInfo = <div className="row" style={{ backgroundColor: 'rgba(255, 0, 0, 0.3)' }}>
-      <p>{obj.data.delegator} withdrew stake worth {obj.data.amount / 1000000000000000000} LPT in round {obj.data.withdrawRound}</p>
+      <p>{obj.data.delegator} withdrew stake worth {(obj.data.amount / 1000000000000000000).toFixed(4)} LPT in round {obj.data.withdrawRound}</p>
     </div>
   } else if (obj.name == "Reward") {
     eventSpecificInfo = <div className="row" style={{ backgroundColor: 'rgba(0, 179, 221, 0.3)' }}>
-      <p>O {obj.data.transcoder} called reward for {obj.data.amount / 1000000000000000000} LPT</p>
+      <p>O {obj.data.transcoder} called reward for {(obj.data.amount / 1000000000000000000).toFixed(4)} LPT</p>
     </div>
   } else if (obj.name == "TranscoderUpdate") {
-    eventSpecificInfo = <div className="row" style={{ backgroundColor: 'rgba(245, 5, 89, 0.3)' }}>
+    eventSpecificInfo = <div className="row" style={{ backgroundColor: 'rgba(215, 15, 0, 0.3)' }}>
       <p>O {obj.data.transcoder} changed their rewardCut to {obj.data.rewardCut} and their feeShare to {obj.data.feeShare}</p>
     </div>
   } else if (obj.name == "TranscoderActivated") {
