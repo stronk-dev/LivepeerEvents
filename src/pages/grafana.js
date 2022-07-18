6efa0c4e1c1b69a5e1f51e6512de1e614801b2b1
Marco van Dijk <marco@stronk.rocks>
2022-07-19 00:28:14 +0200

    Modified Grafana page

--- src/pages/grafana.js
+++ src/pages/grafana.js
@@ -14,21 +14,7 @@ const Grafana = (obj) => {
   if (redirectToHome) {
     return <Navigate push to="/" />;
   }
-  // Generate component for displaying LPT and ETH price and price change
-  let lptPrice = 0;
-  let ethPrice = 0;
-  let lptPriceChange24h = 0;
-  let ethPriceChange24h = 0;
-  if (livepeer.quotes) {
-    if (livepeer.quotes.LPT) {
-      lptPrice = livepeer.quotes.LPT.price.toFixed(2);
-      lptPriceChange24h = livepeer.quotes.LPT.percent_change_24h.toFixed(2);
-    }
-    if (livepeer.quotes.ETH) {
-      ethPrice = livepeer.quotes.ETH.price.toFixed(2);
-      ethPriceChange24h = livepeer.quotes.ETH.percent_change_24h.toFixed(2);
-    }
-  }
+
 
   return (
     <div className="stroke" >
@@ -47,24 +33,18 @@ const Grafana = (obj) => {
             <div className="stroke roundedOpaque">
               <div className="verticalDivider" />
               <div className="flexContainer">
-                <div className="row">
-                  <img alt="" src="livepeer.png" width="30" height="30" />
-                  <p>${lptPrice}</p>
-                  <p>({lptPriceChange24h}%)</p>
-                </div>
                 <div className="row" onClick={() => {
                   setRedirectToHome(true);
                 }}>
                   <h2>Livepeer Orchestrator</h2>
                 </div>
-                <div className="row">
-                  <img alt="" src="eth.png" width="30" height="30" />
-                  <p>${ethPrice}</p>
-                  <p>({ethPriceChange24h}%)</p>
-                </div>
               </div>
               <div className="verticalDivider" />
               <div className="stroke roundedOpaqueDark">
+                <div className="flexContainer stretchAndPad">
+                  <iframe className="halfGrafana" src="https://grafana.stronk.tech/d-solo/71b6OZ0Gz/orchestrator-overview?orgId=1&refresh=15s&theme=dark&panelId=75" height="200" frameBorder="0"></iframe>
+                  <iframe className="halfGrafana" src="https://grafana.stronk.tech/d-solo/71b6OZ0Gz/orchestrator-overview?orgId=1&refresh=15s&theme=dark&panelId=23763572196" height="200" frameBorder="0"></iframe>
+                </div>
                 <div className="flexContainer stretchAndPad">
                   <iframe className="halfGrafana" src="https://grafana.stronk.tech/d-solo/71b6OZ0Gz/orchestrator-overview?orgId=1&refresh=15s&theme=dark&panelId=79" height="200" frameBorder="0"></iframe>
                   <iframe className="halfGrafana" src="https://grafana.stronk.tech/d-solo/71b6OZ0Gz/orchestrator-overview?orgId=1&refresh=15s&theme=dark&panelId=83" height="200" frameBorder="0"></iframe>
@@ -72,7 +52,7 @@ const Grafana = (obj) => {
                 <div className="row" style={{ width: 'unset', marginTop: '1em', marginBottom: '1em' }}>
                   <a className="selectOrch" style={{ padding: '0.2em', cursor: 'alias' }} target="_blank" rel="noopener noreferrer" href={"https://explorer.livepeer.org/accounts/0x847791cbf03be716a7fe9dc8c9affe17bd49ae5e/delegating"} >
                     <img alt="" src="livepeer.png" width="20em" height="20em" />
-                    <span className="lightTextAlt">Stake with Captain Chad</span>
+                    <span className="lightTextAlt">Stake with captain-chad.eth</span>
                   </a>
                 </div>
                 <div className="flexContainer stretchAndPad">
@@ -84,7 +64,7 @@ const Grafana = (obj) => {
                 <div className="row" style={{ width: 'unset', marginTop: '1em', marginBottom: '1em' }}>
                   <a className="selectOrch" style={{ padding: '0.2em', cursor: 'alias' }} target="_blank" rel="noopener noreferrer" href={"https://explorer.livepeer.org/accounts/0x847791cbf03be716a7fe9dc8c9affe17bd49ae5e/delegating"} >
                     <img alt="" src="livepeer.png" width="20em" height="20em" />
-                    <span className="lightTextAlt">Stake with Captain Chad</span>
+                    <span className="lightTextAlt">Stake with captain-chad.eth</span>
                   </a>
                 </div>
                 <div className="flexContainer stretchAndPad">
@@ -96,7 +76,7 @@ const Grafana = (obj) => {
                 <div className="row" style={{ width: 'unset', marginTop: '1em', marginBottom: '1em' }}>
                   <a className="selectOrch" style={{ padding: '0.2em', cursor: 'alias' }} target="_blank" rel="noopener noreferrer" href={"https://explorer.livepeer.org/accounts/0x847791cbf03be716a7fe9dc8c9affe17bd49ae5e/delegating"} >
                     <img alt="" src="livepeer.png" width="20em" height="20em" />
-                    <span className="lightTextAlt">Stake with Captain Chad</span>
+                    <span className="lightTextAlt">Stake with captain-chad.eth</span>
                   </a>
                 </div>
                 <div className="flexContainer stretchAndPad">
@@ -108,7 +88,7 @@ const Grafana = (obj) => {
                 <div className="row" style={{ width: 'unset', marginTop: '1em', marginBottom: '1em' }}>
                   <a className="selectOrch" style={{ padding: '0.2em', cursor: 'alias' }} target="_blank" rel="noopener noreferrer" href={"https://explorer.livepeer.org/accounts/0x847791cbf03be716a7fe9dc8c9affe17bd49ae5e/delegating"} >
                     <img alt="" src="livepeer.png" width="20em" height="20em" />
-                    <span className="lightTextAlt">Stake with Captain Chad</span>
+                    <span className="lightTextAlt">Stake with captain-chad.eth</span>
                   </a>
                 </div>
                 <div className="flexContainer stretchAndPad">
@@ -120,7 +100,7 @@ const Grafana = (obj) => {
                 <div className="row" style={{ width: 'unset', marginTop: '1em', marginBottom: '1em' }}>
                   <a className="selectOrch" style={{ padding: '0.2em', cursor: 'alias' }} target="_blank" rel="noopener noreferrer" href={"https://explorer.livepeer.org/accounts/0x847791cbf03be716a7fe9dc8c9affe17bd49ae5e/delegating"} >
                     <img alt="" src="livepeer.png" width="20em" height="20em" />
-                    <span className="lightTextAlt">Stake with Captain Chad</span>
+                    <span className="lightTextAlt">Stake with captain-chad.eth</span>
                   </a>
                 </div>
               </div>
